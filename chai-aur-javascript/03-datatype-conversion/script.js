5fec242483f1111c9e7b67c6c8a2a851df567ef7
Yuvraj Shrirame <137785595+yuvrajshrirame@users.noreply.github.com>
2024-05-06 06:59:19 +0000

    Update script.js

--- chai-aur-javascript/03-datatype-conversion/script.js
+++ chai-aur-javascript/03-datatype-conversion/script.js
@@ -18,22 +18,64 @@
 
 // CONVERSIONS
 
-// "33" => 33
-// "33abc" => NaN
-// true => 1 ; false => 0
+    // "33" => 33
+    // "33abc" => NaN
+    // true => 1 ; false => 0
 
-let isLoggedIn = 1;
+// let isLoggedIn = 1;
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 2 => false
 // empty string "" => true
 // string "yuvraj" => false
 
-let someNum = 23;
+// let someNum = 23;
 
-let strNum = String(someNum);
-console.log(strNum);
-console.log(typeof strNum);
+// let strNum = String(someNum);
+// console.log(strNum);
+// console.log(typeof strNum);
+
+//*********************OPERATIONS***********************//
+
+// let val = 100;
+// let negVal = -val;
+// console.log(negVal); // -100
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "hello";
+// let str2 = "yuvraj";
+// let str3 = str1 + str2;
+// console.log(str3); // helloyuvraj
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 1 + 2); // 112
+// console.log(1 + 2 + "2"); // 32
+
+// **** TRUE AND FALSE **** //
+
+    // console.log(true); // true
+    // console.log(false); // false
+    // console.log(+true); // 1
+    // console.log(+false); // 2
+
+    let num1, num2, num3;
+    num1 = num2 = num3 = 2 + 2;
+    console.log(num1, num2, num3); // 4 4 4
+    
+    // **** INCREMENTS / DECREMENTS **** //
+    
+    let gameCounter = 100;
+    gameCounter++;
+    console.log(gameCounter);
+    ++gameCounter;
+    console.log(gameCounter);
